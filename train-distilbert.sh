064b1e2a5499c5446b543f1ad4e78d26ffcccc5b
dsindex <hazzling@gmail.com>
2020-06-05 22:51:06 +0900

    -

---
+++ train-distilbert.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+
+VERBOSE_MODE=0
+
+function error_handler()
+{
+  local STATUS=${1:-1}
+  [ ${VERBOSE_MODE} == 0 ] && exit ${STATUS}
+  echo "Exits abnormally at line "`caller 0`
+  exit ${STATUS}
+}
+trap "error_handler" ERR
+
+PROGNAME=`basename ${BASH_SOURCE}`
+DRY_RUN_MODE=0
+
+function print_usage_and_exit()
+{
+  set +x
+  local STATUS=$1
+  echo "Usage: ${PROGNAME} [-v] [-v] [--dry-run] [-h] [--help]"
+  echo ""
+  echo " Options -"
+  echo "  -v                 enables verbose mode 1"
+  echo "  -v -v              enables verbose mode 2"
+  echo "      --dry-run      show what would have been dumped"
+  echo "  -h, --help         shows this help message"
+  exit ${STATUS:-0}
+}
+
+function debug()
+{
+  if [ "$VERBOSE_MODE" != 0 ]; then
+    echo $@
+  fi
+}
+
+#GETOPT=`getopt -o vh --long dry-run,help -n "${PROGNAME}" -- "$@"`
+GETOPT=`getopt vh $*`
+if [ $? != 0 ] ; then print_usage_and_exit 1; fi
+
+eval set -- "${GETOPT}"
+
+while true
+do case "$1" in
+     -v)            let VERBOSE_MODE+=1; shift;;
+     --dry-run)     DRY_RUN_MODE=1; shift;;
+     -h|--help)     print_usage_and_exit 0;;
+     --)            shift; break;;
+     *) echo "Internal error!"; exit 1;;
+   esac
+done
+
+if (( VERBOSE_MODE > 1 )); then
+  set -x
+fi
+
+if [ ${#} != 0 ]; then print_usage_and_exit 1; fi
+
+set -o errexit
+function readlink()
+{
+    TARGET_FILE=$2
+    cd `dirname $TARGET_FILE`
+    TARGET_FILE=`basename $TARGET_FILE`
+
+    # Iterate down a (possible) chain of symlinks
+    while [ -L "$TARGET_FILE" ]
+    do
+        TARGET_FILE=`readlink $TARGET_FILE`
+        cd `dirname $TARGET_FILE`
+        TARGET_FILE=`basename $TARGET_FILE`
+    done
+
+    # Compute the canonicalized name by finding the physical path
+    # for the directory we're in and appending the target file.
+    PHYS_DIR=`pwd -P`
+    RESULT=$PHYS_DIR/$TARGET_FILE
+    echo $RESULT
+}
+export -f readlink
+
+# current dir of this script
+CDIR=$(readlink -f $(dirname $(readlink -f ${BASH_SOURCE[0]})))
+PDIR=$(readlink -f $(dirname $(readlink -f ${BASH_SOURCE[0]}))/..)
+
+
+
+# binarize
+cd ${CDIR}
+FILE_PATH=korean/all.txt
+TOK_NAME=pytorch.all.bpe.4.8m_step 
+DUMP_PATH=korean/binarized_text
+python distillation/scripts/binarized_data.py \
+    --file_path      ${FILE_PATH} \
+    --tokenizer_type bert \
+    --tokenizer_name ${TOK_NAME} \
+    --dump_file      ${DUMP_PATH}
